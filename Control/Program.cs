eb8a72f1d01c08449608340ebc9b2e8cb1e590c6
DenI-10 <121663755+DenI-10@users.noreply.github.com>
2023-02-26 18:48:23 +0300

    Added a diagram

---
+++ Control/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] firstArray = new string[5] { "43", "11", "array", "python", "add" };
+string[] secondArray = new string[firstArray.Length];
+
+void FormingArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FormingArray(firstArray, secondArray);
+PrintArray(secondArray);
